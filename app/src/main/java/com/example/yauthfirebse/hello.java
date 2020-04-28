3ff01f36aeb4b011baed53d6fdcb46663a1f4db4
PRONOY <pronoyroy687@gmail.com>
2020-04-29 00:36:21 +0600

    Authentication

---
+++ app/src/main/java/com/example/yauthfirebse/hello.java
@@ -0,0 +1,41 @@
+package com.example.yauthfirebse;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class hello extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_layout,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.signout){
+
+     FirebaseAuth.getInstance().signOut();
+     finish();
+            Intent intent =new Intent(hello.this,MainActivity.class);
+            startActivity(intent);
+        }
+        return super.onOptionsItemSelected(item);
+
+    }
+}
